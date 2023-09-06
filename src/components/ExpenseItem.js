2d422e6aef946ebed9cb10adcc78b26a0691888d
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-06 15:48:20 +0530

    1

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './ExpenseItem.css';
 
 const ExpenseItem = () => {
+
+    const expenseDate = new Date(2023, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;
     return (
         <div className='expense-item'>
             <div>September 6th 2023</div>
             <div className='expense-item__description'>
-                <h2>Car Insurance</h2>
+                <h2>{expenseTitle}</h2>
                 <div className='expense-item__price'>$294.67</div>
             </div>
         </div>
