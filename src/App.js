6ac75baabf4feff13aa71a5f0298b396948154ea
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-06 15:31:42 +0530

    expenses added

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+
+function App() {
+  return (
+    <>
+      <h2>Let's get started!</h2>
+      <ExpenseItem />
+      <ExpenseItem />
+      <ExpenseItem />
+    </>
+  );
+}
+
+export default App;
