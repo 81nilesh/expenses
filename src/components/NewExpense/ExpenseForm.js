db3519955effff04e069000cb8709b8009e2d940
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-07 11:57:58 +0530

    Advance React10

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -41,9 +41,20 @@ const ExpenseForm = () => {
         //     enteredDate: event.target.value,
         // })
     };
+
+    const submitHandler = (event) => {
+        event.preventDefault();
+
+        const expenseData = {
+            title: enteredTitle,
+            amount: enteredAmount,
+            date: new Date(enteredDate)
+        };
+        console.log(expenseData);
+    };
     return (
         <div>
-            <form>
+            <form onSubmit={submitHandler}>
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
