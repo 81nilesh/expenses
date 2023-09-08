cf99f288fe2fdb0d713c5254b8d3b087ab964128
ankitchoudhary8120@gmail.com <ankitchoudhary8120@gmail.com>
2023-09-08 16:46:46 +0530

    Advance React11

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -51,6 +51,9 @@ const ExpenseForm = () => {
             date: new Date(enteredDate)
         };
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     };
     return (
         <div>
@@ -58,15 +61,25 @@ const ExpenseForm = () => {
                 <div className='new-expense__controls'>
                     <div className='new-expense__control'>
                         <label>Title</label>
-                        <input type="text" onChange={titleChangeHandler} />
+                        <input type="text"
+                            value={enteredTitle}
+                            onChange={titleChangeHandler} />
                     </div>
                     <div className='new-expense__control'>
                         <label>Amount</label>
-                        <input type="number" min='0.01' step='0.01' onChange={amountChangeHandler} />
+                        <input type="number"
+                            min='0.01'
+                            step='0.01'
+                            value={enteredAmount}
+                            onChange={amountChangeHandler} />
                     </div>
                     <div className='new-expense__control'>
-                        <label>Title</label>
-                        <input type="date" min='2019-01-01' max='2023-09-07' onChange={dateChangeHandler} />
+                        <label>Date</label>
+                        <input type="date"
+                            min='2019-01-01'
+                            max='2023-09-07'
+                            value={enteredDate}
+                            onChange={dateChangeHandler} />
                     </div>
                 </div>
                 <div className='new-expense__actions'>
